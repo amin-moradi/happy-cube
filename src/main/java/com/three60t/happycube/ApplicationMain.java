37726e0cbde15e0c4fcc402b27600c8c18daca94
Hossein Moradi <h.moradi20@gmail.com>
2019-03-17 18:19:44 +0330

    add logger and use RedCube as input data

--- src/main/java/com/three60t/happycube/ApplicationMain.java
+++ src/main/java/com/three60t/happycube/ApplicationMain.java
@@ -1,7 +1,9 @@
 package com.three60t.happycube;
 
-import com.three60t.happycube.cube.BlueCube;
 import com.three60t.happycube.cube.Cube;
+import com.three60t.happycube.cube.RedCube;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 
 /**
@@ -9,8 +11,14 @@ import com.three60t.happycube.cube.Cube;
  * @since 3/15/19
  */
 public class ApplicationMain {
+    private static Logger logger = Logger.getLogger(ApplicationMain.class.getName());
+
     public static void main(String[] args) {
-        Cube blueCube = new BlueCube();
-        blueCube.findSolutions();
+        BasicConfigurator.configure();
+
+        Cube blueCube = new RedCube();
+        if (blueCube.findSolutions().size() == 0) {
+            logger.debug("Nothing found!");
+        }
     }
 }
